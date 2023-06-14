fd3a591a1f77d57b6a884ed945069d0bb1b4da08
Somesh <someshverma8055@gmail.com>
2023-06-14 13:51:07 +0530

    gmail

--- src/Components/Topbar/Topbar.js
+++ src/Components/Topbar/Topbar.js
@@ -4,7 +4,7 @@ import "./Topbar.css";
 export default function Topbar() {
   return (
     <header>
-      <h3>S O M E S H</h3>
+      <h3>S O M E S H </h3>
       <nav>
         <a href="#home">Home</a>
         <a href="#services">Services</a>
